b05e7f205675141561d0741c92cddee92433a193
Fabiano Santana Silva <fabiano.santana@epcbhz.local>
2018-04-06 15:33:48 -0300

    App apenas com react

---
+++ client/src/about/about.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum</p>
+    </div>
+)
---
+++ client/src/todo/todo.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react"
+import PageHeader from '../template/pageHeader'
+
+export default class Todo extends Component {
+    render() {
+        return (
+            <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
+        )
+    }
+}
